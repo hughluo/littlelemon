3e6be4d5cc3f4eed4a4a995458c10dd4f9f065a4
Yinchi Luo <bugwriter@outlook.com>
2023-02-12 21:25:05 +0100

    Initial Commit

---
+++ LittleLemmon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LittleLemmon
+//
+//  Created by Yinchi Luo on 12.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LittleLemmon/LittleLemmonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemmonApp.swift
+//  LittleLemmon
+//
+//  Created by Yinchi Luo on 12.02.23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemmonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
